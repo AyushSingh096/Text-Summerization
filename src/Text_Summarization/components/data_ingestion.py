18f0990a3f835d1ea9d08842bdf921cbadcadc0b
AyushSingh096 <ayushsingh00112233@gmail.com>
2023-12-25 22:43:52 +0530

    till data_ingestion

---
+++ src/Text_Summarization/components/data_ingestion.py
@@ -0,0 +1,33 @@
+import os
+import urllib.request as request
+import zipfile
+from Text_Summarization.logging import logger
+from Text_Summarization.utils.common import get_size
+from Text_Summarization.entity import DataIngestionConfig
+from pathlib import Path
+
+
+class DataIngestion:
+    def __init__(self,config: DataIngestionConfig):
+        self.config = config
+
+    def download_file(self):
+        if not os.path.exists(self.config.local_data_file):
+            filename, headers  = request.urlretrieve(
+                url = self.config.source_URL,
+                filename = self.config.local_data_file
+            )
+            logger.info(f"{filename} download! with following info: \n{headers}")
+        else:
+            logger.info(f" File already exists of same size: get_size(path(self.config.local_data_file))")
+    
+    def extract_zip_file(self):
+        """ 
+        zip_file_path : str
+        Extract the zip file into data directory
+        function returns None
+        """
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok= True)
+        with zipfile.ZipFile(self.config.local_data_file, "r") as zip_ref:
+            zip_ref.extractall(unzip_path)
