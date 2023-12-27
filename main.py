0368c729940ff8b2f9b5e33702edb4bc95db00e0
AyushSingh096 <ayushsingh00112233@gmail.com>
2023-12-28 00:46:55 +0530

    added model_trainer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Text_Summarization.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Text_Summarization.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from Text_Summarization.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from Text_Summarization.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 from Text_Summarization.logging import logger
 
 
@@ -31,6 +32,17 @@ try:
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<\n\nx===========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Trainer Stage"
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<\n\nx===========x")
 except Exception as e:
     logger.exception(e)
